41725015921a2b59db0a53e7891d863a7f29a7b7
aliraza114 <aliraza3569@gmail.com>
2020-12-02 22:43:16 +0500

    mongoose added model created

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 // pkg imports
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 // local imports
 const feedRoutes = require('./routes/feed')
 
+const MONGODB_URI = 'mongodb+srv://aliraza:aliraza@cluster0.g6cnw.mongodb.net/onlineshop'
+
 const app = express()
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Orign', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
 
 //registering routes
 app.use('/feed', feedRoutes)
 
-app.listen(8080)
+mongoose.connect(MONGODB_URI).then(
+    app.listen(8080)
+).catch(err => console.log(err))
--- controller/feed.js
+++ controller/feed.js
@@ -1,3 +1,39 @@
+const { validationResult } = require('express-validator/check')
+
 exports.getPosts = (req, res, next) => {
-    
+    res.status(200).json({
+        posts: {
+            _id: new Date().toISOString(),
+            title: 'First Post',
+            content: 'Content of the post!',
+            imageUrl: 'images/test',
+            creator: {
+                name: 'Ali Raza'
+            },
+            createdAt: new Date()
+        }
+    })
 }
+
+exports.createPost = ((req, res, next) =>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty){
+        return res.status(422).json({
+            message: "Validation failed, entered data is incorrect",
+            errors: errors.array()
+        })
+    }
+    res.status(200).json({
+        message: 'Post Created Successflly',
+        posts: {
+            _id: new Date().toISOString(),
+            title: 'Another Post',
+            content: 'Content of the post 2!',
+            imageUrl: 'images/test',
+            creator: {
+                name: 'Ali Raza 2'
+            },
+            createdAt: new Date()
+        }
+    })
+}) 
