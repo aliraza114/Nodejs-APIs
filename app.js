72f4ae8b4dbb23484a663e803c291ca0c10730f3
aliraza35690 <aliraza3569@gmail.com>
2020-12-09 23:27:01 +0500

    socket.io added

--- app.js
+++ app.js
@@ -59,6 +59,10 @@ app.use((err, req, res, next) =>{
     })
 })
 
-mongoose.connect(MONGODB_URI).then(
-    app.listen(8080)
-).catch(err => console.log(err))
+mongoose.connect(MONGODB_URI).then( result => {
+    const server = app.listen(8080)
+    const io = require('./socket').init(server)
+    io.on('connection', socket =>{
+        console.log('Client Connected!')
+    })
+}).catch(err => console.log(err))
